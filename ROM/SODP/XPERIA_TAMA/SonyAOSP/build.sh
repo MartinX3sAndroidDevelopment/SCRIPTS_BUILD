fd1e6eae786331f61bfb0fcb0cadaebf1dfadc53
Martin <nc-duenkekl3@netcologne.de>
2024-11-09 16:46:28 +0100

    feat(SonyAOSP): adapt to the A15 build artifact filenames

--- ROM/SODP/XPERIA_TAMA/SonyAOSP/build.sh
+++ ROM/SODP/XPERIA_TAMA/SonyAOSP/build.sh
@@ -15,14 +15,14 @@ function add_custom_hacks() {
 
 function build_sonyAOSP() {
     echo "####SONY AOSP BUILD START####"
-    echo "####$1 Sim START####"
-    functions_build_customROM_helper ${customROM_dir:?} "${1:?}"-userdebug
+    echo "####$1-$2 Sim START####"
+    functions_build_customROM_helper ${customROM_dir:?} "${1:?}"-"${2:?}"-userdebug
 
     cd ${customROM_dir:?}
-    echo "Log build to ${build_out}/aosp-${aosp_version}_""${2:?}""-build.log"
-    make -j dist > ${build_out}/aosp-${aosp_version}_"${2:?}"-build.log
-    mv out/dist/"${1:?}"-ota-eng.*.zip ${build_out}/aosp-${aosp_version}-"$(date +%Y%m%d)"_"${2:?}".zip
-    echo "####$1 Sim END####"
+    echo "Log build to ${build_out}/aosp-${aosp_version}_""${3:?}""-build.log"
+    make -j dist > ${build_out}/aosp-${aosp_version}_"${3:?}"-build.log
+    mv out/dist/"${1:?}"-ota-*.zip ${build_out}/aosp-${aosp_version}-"$(date +%Y%m%d)"_"${3:?}".zip
+    echo "####$1-$2 Sim END####"
     echo "####SONY AOSP BUILD END####"
 }
 
@@ -60,8 +60,8 @@ functions_update_customROM_WORKAROUND ${customROM_dir:?}  # TODO Temp until the 
 
 add_custom_hacks
 
-build_sonyAOSP aosp_h8216-ap3a akari # XZ2_SS
-build_sonyAOSP aosp_h8416-ap3a akatsuki # XZ3_SS
+build_sonyAOSP aosp_h8216 ap3a akari # XZ2_SS
+build_sonyAOSP aosp_h8416 ap3a akatsuki # XZ3_SS
 
 functions_success "Sony AOSP"
 
